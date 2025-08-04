7b10b03915b6e1d4e144bc5ab0d503b8a122246d
Daniel Mezker <dmezker06@gmail.com>
2025-08-04 12:24:49 +0200

    Added Function

--- CI_CD_Project_with_Unit_Tests/CI_CD_Project_with_Unit_Tests/Program.cs
+++ CI_CD_Project_with_Unit_Tests/CI_CD_Project_with_Unit_Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace CI_CD_Project_with_Unit_Tests
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Gebe bitte deine erste Zahl ein, welche du addieren möchtest");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gebe bitte deine zweite Zahl ein, welche du addieren möchtest");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            int sum = zahl1 + zahl2;
+
+            Console.WriteLine("Das Ergbnis ist: " + sum);
         }
     }
 }
